6c5a2c98f0d42072a51cd3e72566b36b1c187928
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-21 11:14:04 +0100

    added

---
+++ chatty/chatty/chattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chattyApp.swift
+//  chatty
+//
+//  Created by Mac on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct chattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
